c82d3f89df9eda772fc27b8a541fad9b09c33f41
Alex <aliaksandrl@expressdecor.com>
2012-01-03 15:27:25 -0500

    added Iceberg module

---
+++ app/code/community/IcebergCommerce/WidgetTagProducts/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Iceberg Commerce
+ * @author     IcebergCommerce
+ * @package    IcebergCommerce_WidgetTagProducts
+ * @copyright  Copyright (c) 2010 Iceberg Commerce
+ */
+class IcebergCommerce_WidgetTagProducts_Helper_Data extends Mage_Core_Helper_Abstract
+{
+	
+}
