d8c6003bfd479bb7d107b8d47a2c70c6309a72f8
Alex on dev <aliaksandrl@expressdecor.com>
2012-02-06 13:49:41 -0500

    test

--- set_session.php
+++ set_session.php
@@ -2,6 +2,7 @@
 require_once 'app/Mage.php';
 Mage::run('','store');
 
+
 Mage::getSingleton("core/session", array("name" => "frontend"));
 $open=Mage::app()->getRequest()->getParam('customer_show');
 Mage::getModel('core/session')->setNeedHelp($open);
