abb3e365cb39a6534d2cee9ce5d90fda02dc49c2
Alex on dev <aliaksandrl@expressdecor.com>
2012-06-21 09:47:57 -0400

    Fixed Task #66939 http://pm.kraususa.net/index.php?c=task&a=view&id=66939

--- app/code/local/Expressdecor/Ordercomments/Model/Sales/Order.php
+++ app/code/local/Expressdecor/Ordercomments/Model/Sales/Order.php
@@ -4,9 +4,78 @@
 class Expressdecor_Ordercomments_Model_Sales_Order extends Mage_Sales_Model_Order
 
 {
+	/**
+	 * Send email with order update information
+	 *
+	 * @param boolean $notifyCustomer
+	 * @param string $comment
+	 * @return Mage_Sales_Model_Order
+	 */
+	public function sendOrderUpdateEmail($notifyCustomer = true, $comment = '')
+	{
+		$storeId = $this->getStore()->getId();
+		$comment=nl2br($comment); // Alex
+		if (!Mage::helper('sales')->canSendOrderCommentEmail($storeId)) {
+			return $this;
+		}
+		// Get the destination email addresses to send copies to
+		$copyTo = $this->_getEmails(self::XML_PATH_UPDATE_EMAIL_COPY_TO);
+		$copyMethod = Mage::getStoreConfig(self::XML_PATH_UPDATE_EMAIL_COPY_METHOD, $storeId);
+		// Check if at least one recepient is found
+		if (!$notifyCustomer && !$copyTo) {
+			return $this;
+		}
+	
+		// Retrieve corresponding email template id and customer name
+		if ($this->getCustomerIsGuest()) {
+			$templateId = Mage::getStoreConfig(self::XML_PATH_UPDATE_EMAIL_GUEST_TEMPLATE, $storeId);
+			$customerName = $this->getBillingAddress()->getName();
+		} else {
+			$templateId = Mage::getStoreConfig(self::XML_PATH_UPDATE_EMAIL_TEMPLATE, $storeId);
+			$customerName = $this->getCustomerName();
+		}
+	
+		$mailer = Mage::getModel('core/email_template_mailer');
+		if ($notifyCustomer) {
+			$emailInfo = Mage::getModel('core/email_info');
+			$emailInfo->addTo($this->getCustomerEmail(), $customerName);
+			if ($copyTo && $copyMethod == 'bcc') {
+				// Add bcc to customer email
+				foreach ($copyTo as $email) {
+					$emailInfo->addBcc($email);
+				}
+			}
+			$mailer->addEmailInfo($emailInfo);
+		}
+	
+		// Email copies are sent as separated emails if their copy method is
+		// 'copy' or a customer should not be notified
+		if ($copyTo && ($copyMethod == 'copy' || !$notifyCustomer)) {
+			foreach ($copyTo as $email) {
+				$emailInfo = Mage::getModel('core/email_info');
+				$emailInfo->addTo($email);
+				$mailer->addEmailInfo($emailInfo);
+			}
+		}
+	
+		// Set all required params and send emails
+		$mailer->setSender(Mage::getStoreConfig(self::XML_PATH_UPDATE_EMAIL_IDENTITY, $storeId));
+		$mailer->setStoreId($storeId);
+		$mailer->setTemplateId($templateId);
+		$mailer->setTemplateParams(array(
+				'order'   => $this,
+				'comment' => $comment,
+				'billing' => $this->getBillingAddress()
+		)
+		);
+		$mailer->send();
+	
+		return $this;
+	}
 	
 	public function addStatusHistoryComment($comment, $status = false)
 	{
+		$comment=nl2br($comment); //<br/>
 // 		if (false === $status) {
 			$status = $this->getStatus();
 	/*	} elseif (true === $status) {
