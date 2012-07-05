41b76701d6696ab92c4125628a3203af914b2d5d
Alex on dev <aliaksandrl@expressdecor.com>
2012-07-05 14:24:38 -0400

    Tags import

---
+++ tags.php
@@ -0,0 +1,54 @@
+<?php
+$filename="tags.csv";
+$filedata=array();
+$row=1;
+require_once 'app/Mage.php';
+Mage::app('default');
+$storeId = Mage::app()->getStore()->getId();
+
+if (($handle = fopen($filename, "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data); 
+		array_push($filedata, $data);
+    }
+    fclose($handle);
+}
+
+
+//print_r($filedata);
+$model=Mage::getModel("tag/tag");
+ 
+foreach ($filedata as $tag) {
+	// add tags if they don't exists
+	$model->loadByName($tag[1]);
+	if(count($model->getData())==0) {
+		$model->setStatus(1);
+		$model->setName($tag[1]);
+		$model->save();
+	}
+	
+	$prod_id=Mage::getModel('catalog/product')->getIdBySku($tag[0]);
+ 	$tag_id=$model->loadByName($tag[1])->getId();
+//	print_r($model->getRelatedProductIds());
+	$exists=0;
+	
+	if (count($model->getRelatedProductIds())>0) {
+		foreach ($model->getRelatedProductIds() as $pr_id) {
+			 if ($prod_id==$pr_id) {
+			 	$exists=1;
+			 }
+		}
+	}
+	// if relation didn't exists
+		if ($exists==0) {					if (!empty($prod_id)){
+		$tag_col=Mage::getModel('tag/tag_relation')
+		->setTagId($tag_id) 
+		->setProductId($prod_id)
+		->setStoreId($storeId)
+		->setCustomerId(null)
+		->setActive(1)
+		->save();			} else {				echo $tag[0]."  Product doesn't exist in magento ! <br/>"; 			}
+		}		
+}
+echo "Finished";
+?>
