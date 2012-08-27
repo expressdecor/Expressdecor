dd0b40ce9c5775ee348fb1206a801b020dfad421
Alex on dev <aliaksandrl@expressdecor.com>
2012-08-27 09:24:32 -0400

    left NEED help window. Feedback corrections

--- feedback.php
+++ feedback.php
@@ -7,15 +7,20 @@ $post = Mage::app()->getRequest()->getPost();
 if ( $post ) {
 	$postObject = new Varien_Object();
 	$postObject->setData($post);
-
+	$email_template=Mage::getStoreConfig('contacts/email/email_template');
+	$sender_email_identity=Mage::getStoreConfig('contacts/email/sender_email_identity');
+	$recipient_email=Mage::getStoreConfig('contacts/email/recipient_email');
+	$from=array('name'=>'Feedback', 'email'=>$post['email']);
+ 
 	$mailTemplate = Mage::getModel('core/email_template');
+ 
 	/* @var $mailTemplate Mage_Core_Model_Email_Template */
 	$mailTemplate->setDesignConfig(array('area' => 'frontend'))
 	->setReplyTo($post['email'])
 	->sendTransactional(
-	Mage::getStoreConfig('contacts/email/email_template'),
-	Mage::getStoreConfig('contacts/email/sender_email_identity'),
-	Mage::getStoreConfig('contacts/email/recipient_email'),
+	$email_template,
+	$from,
+	$recipient_email,
 	null,
 	array('data' => $postObject)
 	);
@@ -25,6 +30,25 @@ if ( $post ) {
 	} else
 	{
 		echo "Good";
+		//Sent Email to Admins		
+		$mailTemplate->setDesignConfig(array('area' => 'frontend'))
+		->setReplyTo($post['email']) 
+		->sendTransactional(
+				$email_template,
+				 $from,
+				'alukyanov@expressdecor.com',
+				null,
+				array('data' => $postObject)
+		);
+		$mailTemplate->setDesignConfig(array('area' => 'frontend'))
+		->setReplyTo($post['email'])
+		->sendTransactional(
+				$email_template,
+				$from,
+				'kbedi@expressdecor.com',
+				null,
+				array('data' => $postObject)
+		);
 	}
 } else {
 	echo "No data";
