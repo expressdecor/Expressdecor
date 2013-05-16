9be85e98172752208a7b03a740f838285a7482e1
Alex on dev <aliaksandrl@expressdecor.com>
2013-05-16 15:29:35 -0400

    Task #502 Automatic Inventory Upload 2.1

--- inventory_updater.php
+++ inventory_updater.php
@@ -1,6 +1,8 @@
 <?php 
 ini_set ( "memory_limit", "2048M" );
 set_time_limit ( 0 );
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
 require_once 'app/Mage.php';
 Mage::app ( 'default' );
